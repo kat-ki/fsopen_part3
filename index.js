5fd090973608ffcfc6f5fcc32c63234cb733211d
kat-ki <glraskat@gmail.com>
2024-03-02 16:40:27 +0100

    feat: 3.18 updated info route

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-require('dotenv').config()
 morgan.token('body', request => JSON.stringify(request.body))
-
-const app = express();
 const Person = require('./models/person')
 
+const app = express();
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(morgan(':method :url :body'))
 app.use(express.static('dist'))
-
 const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') {
         return response.status(400).send({error: 'malformatted id'});
     }
     next(error)
 }
+app.use(errorHandler)
+
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
     })
 })
-
 app.get('/info', (request, response) => {
     let newDate = new Date()
-    response.send(`
-<p>Phonebook has info for ${persons.length} people</p>
-<p>${newDate}</p>
-`)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end('PERSON NOT FOUND')
-        }
+    Person.find({}).then(persons => {
+        response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${newDate}</p>`)
     })
+})
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end('PERSON NOT FOUND')
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -92,7 +91,6 @@ app.put('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
