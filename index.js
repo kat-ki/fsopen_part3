c7d3f391c7ea36982ed76eff50d51802aea7fcd7
kat-ki <glraskat@gmail.com>
2024-03-04 17:37:57 +0100

    feat: 3.19 added validation for name length, expanded frontend to fit it

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(express.static('dist'))
 const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') {
         return response.status(400).send({error: 'malformatted id'});
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
     next(error)
 }
@@ -48,7 +50,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     if (body.name === undefined) {
         return response.status(400).json({
@@ -72,9 +74,14 @@ app.post('/api/persons', (request, response) => {
         name: body.name,
         number: body.number
     })
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => {
+            next(error)
+        })
+
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
